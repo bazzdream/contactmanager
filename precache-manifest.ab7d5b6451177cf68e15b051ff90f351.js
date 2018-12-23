a3387e22d4eafc8a21346724d9e53d6568df1ce9
Oliver Barabas <bazzdream@gmail.com>
2018-12-23 17:50:22 +0100

    Updates

---
+++ precache-manifest.ab7d5b6451177cf68e15b051ff90f351.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/contactmanager/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "4b3a0fc2df4a9ca4f92e",
+    "url": "/contactmanager/static/css/main.4fc41a1f.chunk.css"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/contactmanager/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "c0ad2c571edfdba9a2a1",
+    "url": "/contactmanager/static/js/1.c0ad2c57.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/contactmanager/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/contactmanager/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/contactmanager/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/contactmanager/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/contactmanager/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "4b3a0fc2df4a9ca4f92e",
+    "url": "/contactmanager/static/js/main.4b3a0fc2.chunk.js"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/contactmanager/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/contactmanager/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/contactmanager/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/contactmanager/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/contactmanager/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/contactmanager/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/contactmanager/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/contactmanager/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "c0ad2c571edfdba9a2a1",
+    "url": "/contactmanager/static/css/1.425093bb.chunk.css"
+  },
+  {
+    "revision": "4845a6bc420a30d3947ecdd14d461548",
+    "url": "/contactmanager/index.html"
+  }
+];
